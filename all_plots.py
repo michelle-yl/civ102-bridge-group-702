19751f70ff72e7552e3ffed31ed61e7c348f658c
michelle-yl <lymichelle355@gmail.com>
2025-11-21 06:54:54 -0500

    [PLOTS] FOS sheets should be good

--- all_plots.py
+++ all_plots.py
@@ -123,11 +123,15 @@ if __name__ == "__main__":
     geometry = {"A1": [(10, 0), 80, 1.27], "A2": [(10, 73.73), 6.27, 1.27], "A3": [(83.73, 73.73), 6.27, 1.27], "A4": [(10, 1.27), 1.27, 72.46], "A5": [(88.73, 1.27), 1.27, 72.46], "A6": [(0, 75), 100, 1.27]}
     loads = [(400/6, 172), (400/6, 348), (400/6, 512), (400/6, 688), (400/6, 852), (400/6, 1028)]
     # loads = [[67.5, 172], [67.5, 348], [67.5, 512], [67.5, 688], [91.0, 852], [91.0, 1028]]
-    span = 1200
+    freight = 500/(1.38+1.1+1)
+    kN = [[freight*1.38,0], [freight*1.38,176], [freight*1, 340], [freight*1, 516], [freight*1.1, 680], [freight*1.1,856]]    
+    # print(kN)
+    span = 1260
+    I_list = [[418480.7, geometry, (0, 1260), 1]]
     level = 1
     
     # sfe_max, sfe_min, bme, flex_comp, flex_tens = init_plots(loads,span, geometry)
-    sfe, bme, comp_e, tens_e, glue_e = init_plots(loads,span, geometry, level)
+    sfe, bme, comp_e, tens_e, glue_e = init_plots(loads, span, geometry, level)
 
     plot_env(sfe, "sfe")
     # plot_env(bme, "bme")
--- fos.py
+++ fos.py
@@ -2,15 +2,17 @@ import matplotlib.pyplot as plt
 from formatting import format_cursor
 import design0 as d0
 
-def plot_env(span, I_list, b):
+def plot_env(span, geometry, b, level):
+    I_list = [[d0.second_moment_of_area(geometry, d0.calculate_centroidal_axis(geometry)), geometry, (0, span), level]]
+    print(I_list)
     labels = [
         "FOS Shear", "FOS Flexural Tension", "FOS Flexural Compression", 
-        "FOS Shear Glue Tabs", "FOS  Shear Sheets", 
+        "FOS Shear Glue Tabs", "FOS Shear Sheets", 
         "FOS Plate Buckling Case 1", "FOS Plate Buckling Case 2",
         "FOS Plate Buckling Case 3", "FOS Plate Buckling Case 4"
     ]
 
-    fos_list = d0.simulation_safety_factors_across_bridge(span, I_list, b)
+    fos_list = d0.simulation_safety_factors_across_bridge(span, I_list, b,"kN", geometry)
     # print(fos_list[8])
     
     env = []
@@ -33,10 +35,13 @@ def plot_env(span, I_list, b):
 
 if __name__ == "__main__":
     geometry = {"A1": [(10, 0), 80, 1.27], "A2": [(88.73, 1.27), 1.27, 72.46], "A3": [(10, 1.27), 1.27, 72.46], "A4": [(83.73, 73.73), 6.27, 1.27], "A5": [(10, 73.73), 6.27, 1.27], "A6": [(0, 75), 100, 1.27]}
+    geometry2 = {"A1": [(10, 0), 80, 1.27], "A2": [(10, 1.27), 1.27, 72.46], "A3": [(85, 1.27), 1.27, 72.46], "A4": [(10, 73.73), 6.27, 1.27], "A5": [(83.73, 73.73), 6.27, 1.27], "A6": [(0, 75), 100, 1.27], "A7": [(0, 77.54), 100, 1.27]}
+
     loads = [[400/6, 172], [400/6, 348], [400/6, 512], [400/6, 688], [400/6, 852], [400/6, 1028]]    # loads = [[67.5, 172], [67.5, 348], [67.5, 512], [67.5, 688], [91.0, 852], [91.0, 1028]]
-    span = 1200
-    I_list = [[418480.7, geometry, (0, 1200), 1]]
-    level = 1
+    freight = 500/(1.38+1.1+1)
+    kN = [[freight*1.38,0], [freight*1.38,176], [freight*1, 340], [freight*1,516], [freight*1.1, 680], [freight*1.1,856]]    
+    span = 1260
+    level = 2
     b = 2.54
-    plot_env(span, I_list, b)
+    plot_env(span, geometry2, b, level)
     
